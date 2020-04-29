e6236654f0c0bbdbfed4277057d1f7d54fc84b2d
broushky <64540899+broushky@users.noreply.github.com>
2020-04-29 17:30:58 +0200

    Add files via upload

--- Scan_lidar.py
+++ Scan_lidar.py
@@ -10,7 +10,6 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import csv
-#import sys
 from mpl_toolkits.mplot3d import Axes3D
 from tfmini_23 import get_dist
 import pickle
@@ -25,18 +24,18 @@ servo2 = 18
 
 #Define variables
 radius = get_dist()
-min_pos_x = 50
-max_pos_x = 130
+min_pos_x = 0
+max_pos_x = 180
 pos_x = min_pos_x
-min_pos_y = 105
-max_pos_y = 60
+min_pos_y = 100
+max_pos_y = 2
 pos_y = min_pos_y
 pos_x_init = (max_pos_x + min_pos_x) / 2
 pos_y_init = (max_pos_y + min_pos_y) / 2
 deg2rad = math.pi / 180
-last_radius = 0
-inc_step_x = 5
-inc_step_y = 3
+altitude = 0
+inc_step_x = 1
+inc_step_y = 1
 delay_x = 0.05
 delay_y = 0.05
 
@@ -55,23 +54,42 @@ def AngleToPWM(angle):
     return start + angle_as_duration_pwm
 
 
+def coord_xyz(pos_x, pos_y):
+
+    radius = get_dist()
+    azimuth = pos_x * deg2rad
+    elevation = pos_y * deg2rad
+    x = radius * math.sin(elevation) * math.cos(azimuth)
+    y = radius * math.sin(elevation) * math.sin(azimuth)
+    z = radius * math.cos(elevation)
+
+    x = round(x, 2)
+    y = round(y, 2)
+    z = round(z, 2)
+    xlist.append(x)
+    ylist.append(y)
+    zlist.append(z)
+
+    return x, y, z, radius
+
+
 # Choice of scan
 scan = int(input("Press 1 and ENTER for 'AUTO scan'\nPress 2 and ENTER for 'MANUAL scan'\n"))
 if scan > 2 or scan < 1 :
     print("Wrong choice, please choose again!")
     scan = int(input("Press 1 and ENTER for 'AUTO scan'\nPress 2 and ENTER for 'MANUAL scan'\n"))
 
-#Loop for duty values
+#Setup values for manual scan
 if scan == 2:
     start_time = time.time()
     #Angles choice for scan
     min_pos_y = float(input("1. Choose a MINIMAL elevation's angle between -22° and 0° and press ENTER:\n"))
-    while min_pos_y > 0 or min_pos_y < -22 :
+    if min_pos_y > 0 or min_pos_y < -22 :
         print("Bad angle, please choose between -22° and 0°")
         min_pos_y = float(input("New angle:"))
 
     max_pos_y = float(input("2. Choose a MAXIMAL elevation's angle between 0° et 88° and press ENTER:\n"))
-    while max_pos_y > 88 or max_pos_y < 0 :
+    if max_pos_y > 88 or max_pos_y < 0 :
         print("Bad angle, please choose between 0° and 88°")
         max_pos_y = float(input("New angle:"))
 
@@ -90,118 +108,22 @@ if scan == 2:
                 pwm = AngleToPWM(pos_x)
                 pi.set_servo_pulsewidth(servo1, pwm)
                 time.sleep(delay_x)
-                #print(pos_x)
-                radius = get_dist()
-                azimuth = pos_x * deg2rad
-                elevation = pos_y * deg2rad
-                x = radius * math.sin(elevation) * math.cos(azimuth)
-                y = radius * math.sin(elevation) * math.sin(azimuth)
-                z = radius * math.cos(elevation)
+                print(coord_xyz(pos_x, pos_y))
 
-                x = round(x, 2)
-                y = round(y, 2)
-                z = round(z, 2)
-
-                #print("x=", x)
-                #print("y=", y)
-                #print("z=", z)
-
-                xlist.append(x)
-                ylist.append(y)
-                zlist.append(z)
-
-                ax.scatter(xlist[j],ylist[j],zlist[j])
-                j+=1
-                plt.show()
-                plt.pause(0.0001)
-
+        pos_y = pos_y - inc_step_y
         duty = AngleToPWM(pos_y)
         pi.set_servo_pulsewidth(servo2, duty)
-        time.sleep(delay_x)
-        pos_y = pos_y - inc_step_y
-        #print(pos_x, pos_y)
-        radius = get_dist()
-        azimuth = pos_x * deg2rad
-        elevation = pos_y * deg2rad
-        x = radius * math.sin(elevation) * math.cos(azimuth)
-        y = radius * math.sin(elevation) * math.sin(azimuth)
-        z = radius * math.cos(elevation)
-
-        x = round(x, 2)
-        y = round(y, 2)
-        z = round(z, 2)
-
-        #print("x=", x)
-        #print("y=", y)
-        #print("z=", z)
-
-        xlist.append(x)
-        ylist.append(y)
-        zlist.append(z)
-
-        ax.scatter(xlist[j],ylist[j],zlist[j])
-        j+=1
-        plt.show()
-        plt.pause(0.0001)
 
         if max_pos_x > min_pos_x:
             for pos_x in np.arange(max_pos_x, min_pos_x, -inc_step_x):
                 pwm = AngleToPWM(pos_x)
                 pi.set_servo_pulsewidth(servo1, pwm)
                 time.sleep(delay_x)
-                #print(pos_x)
-                radius = get_dist()
-                azimuth = pos_x * deg2rad
-                elevation = pos_y * deg2rad
-                x = radius * math.sin(elevation) * math.cos(azimuth)
-                y = radius * math.sin(elevation) * math.sin(azimuth)
-                z = radius * math.cos(elevation)
+                print(coord_xyz(pos_x, pos_y))
 
-                x = round(x, 2)
-                y = round(y, 2)
-                z = round(z, 2)
-
-                #print("x=", x)
-                #print("y=", y)
-                #print("z=", z)
-
-                xlist.append(x)
-                ylist.append(y)
-                zlist.append(z)
-
-                ax.scatter(xlist[j],ylist[j],zlist[j])
-                j+=1
-                plt.show()
-                plt.pause(0.0001)
-
+        pos_y = pos_y - inc_step_y
         duty = AngleToPWM(pos_y)
         pi.set_servo_pulsewidth(servo2, duty)
-        time.sleep(delay_y)
-        pos_y = pos_y - inc_step_y
-        #print(pos_x, pos_y)
-        radius = get_dist()
-        azimuth = pos_x * deg2rad
-        elevation = pos_y * deg2rad
-        x = radius * math.sin(elevation) * math.cos(azimuth)
-        y = radius * math.sin(elevation) * math.sin(azimuth)
-        z = radius * math.cos(elevation)
-
-        x = round(x, 2)
-        y = round(y, 2)
-        z = round(z, 2)
-
-        #print("x=", x)
-        #print("y=", y)
-        #print("z=", z)
-
-        xlist.append(x)
-        ylist.append(y)
-        zlist.append(z)
-
-        ax.scatter(xlist[j],ylist[j],zlist[j])
-        j+=1
-        plt.show()
-        plt.pause(0.0001)
 
     #Go to initial position
     pi.set_servo_pulsewidth(servo1, 1500)
@@ -210,14 +132,10 @@ if scan == 2:
     #Stop signal
     pi.stop()
     print("--- %s seconds ---" % (time.time() - start_time))
+
+# Auto scan
 else:
     start_time = time.time()
-
-    plt.ion() #
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    j=0
-
     pi.set_servo_pulsewidth(servo1, AngleToPWM(pos_x))
     pi.set_servo_pulsewidth(servo2, AngleToPWM(pos_y))
     while pos_y > max_pos_y:
@@ -227,119 +145,22 @@ else:
                 pwm = AngleToPWM(pos_x)
                 pi.set_servo_pulsewidth(servo1, pwm)
                 time.sleep(delay_x)
-                #print(pos_x)
-                radius = get_dist()
-                azimuth = pos_x * deg2rad
-                elevation = pos_y * deg2rad
-                x = radius * math.sin(elevation) * math.cos(azimuth)
-                y = radius * math.sin(elevation) * math.sin(azimuth)
-                z = radius * math.cos(elevation)
+                coord_xyz(pos_x, pos_y)
 
-                x = round(x, 2)
-                y = round(y, 2)
-                z = round(z, 2)
-
-                #print("x=", x)
-                #print("y=", y)
-                #print("z=", z)
-
-                xlist.append(x)
-                ylist.append(y)
-                zlist.append(z)
-
-                ax.scatter(xlist[j],ylist[j],zlist[j])
-                j+=1
-                plt.show()
-                plt.pause(0.0001)
-
+        pos_y = pos_y - inc_step_y
         duty = AngleToPWM(pos_y)
         pi.set_servo_pulsewidth(servo2, duty)
-        time.sleep(delay_y)
-        pos_y = pos_y - inc_step_y
-        print(pos_y)
-        radius = get_dist()
-        azimuth = pos_x * deg2rad
-        elevation = pos_y * deg2rad
-        x = radius * math.sin(elevation) * math.cos(azimuth)
-        y = radius * math.sin(elevation) * math.sin(azimuth)
-        z = radius * math.cos(elevation)
-
-        x = round(x, 2)
-        y = round(y, 2)
-        z = round(z, 2)
-
-        #print("x=", x)
-        #print("y=", y)
-        #print("z=", z)
-
-        xlist.append(x)
-        ylist.append(y)
-        zlist.append(z)
-
-        ax.scatter(xlist[j],ylist[j],zlist[j])
-        j+=1
-        plt.show()
-        plt.pause(0.0001)
 
         if max_pos_x > min_pos_x:
             for pos_x in np.arange(max_pos_x, min_pos_x, -inc_step_x):
                 pwm = AngleToPWM(pos_x)
                 pi.set_servo_pulsewidth(servo1, pwm)
                 time.sleep(delay_x)
-                #print(pos_x)
-                radius = get_dist()
-                azimuth = pos_x * deg2rad
-                elevation = pos_y * deg2rad
-                x = radius * math.sin(elevation) * math.cos(azimuth)
-                y = radius * math.sin(elevation) * math.sin(azimuth)
-                z = radius * math.cos(elevation)
+                coord_xyz(pos_x, pos_y)
 
-                x = round(x, 2)
-                y = round(y, 2)
-                z = round(z, 2)
-
-                #print("x=", x)
-                #print("y=", y)
-                #print("z=", z)
-
-                xlist.append(x)
-                ylist.append(y)
-                zlist.append(z)
-
-                ax.scatter(xlist[j],ylist[j],zlist[j])
-                j+=1
-                plt.show()
-                plt.pause(0.0001)
-
-
+        pos_y = pos_y - inc_step_y
         duty = AngleToPWM(pos_y)
         pi.set_servo_pulsewidth(servo2, duty)
-        time.sleep(delay_y)
-        pos_y = pos_y - inc_step_y
-        #print(pos_y)
-        radius = get_dist()
-        azimuth = pos_x * deg2rad
-        elevation = pos_y * deg2rad
-        x = radius * math.sin(elevation) * math.cos(azimuth)
-        y = radius * math.sin(elevation) * math.sin(azimuth)
-        z = radius * math.cos(elevation)
-
-        x = round(x, 2)
-        y = round(y, 2)
-        z = round(z, 2)
-
-        print("x=", x)
-        print("y=", y)
-        print("z=", z)
-
-        xlist.append(x)
-        ylist.append(y)
-        zlist.append(z)
-
-        ax.scatter(xlist[j],ylist[j],zlist[j])
-        j+=1
-        plt.show()
-        plt.pause(0.0001)
 
     #Go to initial position
     pi.set_servo_pulsewidth(servo1, 1500)
@@ -349,10 +170,7 @@ else:
     pi.stop()
     print("--- %s seconds ---" % (time.time() - start_time))
 
-#print(xlist)
-#print(ylist)
-#print(zlist)
-
+# Save points cloud in csv file
 csvfile=open('Mapping_data.txt','w', newline='')
 obj=csv.writer(csvfile)
 obj.writerow(xlist)
@@ -360,17 +178,13 @@ obj.writerow(ylist)
 obj.writerow(zlist)
 csvfile.close()
 
+# Show 3D points cloud into Mathplotlib
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.set_xlabel('X')
-ax.set_xlim(-400, 400)
+ax.set_xlim(-100, 100)
 ax.set_ylabel('Y')
-ax.set_ylim(-10, 500)
-ax.set_zlabel('Z')
-ax.set_zlim(-100, 400)
-#plt.xlim((-300,300))
-#plt.ylim((-300,300))
-
+ax.set_ylim(-10, 200)
 ax.scatter(xlist, ylist, zlist)
 pickle.dump(fig, open('Mapp_lidar.fig.pickle', 'wb'))
 plt.show()
